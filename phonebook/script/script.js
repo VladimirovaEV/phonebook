aaf6fd705b5cdeb333ce16a5602613b59fdae9a7
Vladimirova Ekaterina <echapyrkina@mail.ru>
2022-06-16 22:39:48 +0300

    задание 7 урока

--- phonebook/script/script.js
+++ phonebook/script/script.js
@@ -21,12 +21,16 @@ const data = [
     phone: '+79876543210',
   },
 ];
-{
+{ 
+  const addContactData = contact => {
+    data.push(contact);
+    console.log('data', data);
+  };
   const createContainer = () => {
     const container = document.createElement('div');
     container.classList.add('container');
     return container;
-  }
+  };
   const createHeader = () => {
     const header = document.createElement('header');
     header.classList.add('header');
@@ -158,12 +162,12 @@ const data = [
       },
     ]);
     const table = createTable();
-    const form = createForm();
+    const {form, overlay} = createForm();
     const footer = createFooter();
     const textDiv = createFooterText(title);
 
     header.headerContainer.append(logo);
-    main.mainContainer.append(buttonGroup.btnWrapper, table, form.overlay);
+    main.mainContainer.append(buttonGroup.btnWrapper, table, overlay);
     app.append(header, main, footer);
     footer.footerContainer.append(textDiv);
     return {
@@ -171,8 +175,7 @@ const data = [
       logo,
       btnAdd: buttonGroup.btns[0],
       btnDel: buttonGroup.btns[1],
-      formOverlay: form.overlay,
-      form: form.form,
+      formOverlay: overlay, form,
     };
   };
   const createRow = ({name: firstName, surname, phone}) => {
@@ -224,31 +227,27 @@ const data = [
       });
     });
   };
-  const init = (selectorApp, title) => {
-    const app = document.querySelector(selectorApp);
-    const phoneBook = renderPhoneBook(app, title);
-    const {
-      list,
-      logo,
-      btnAdd,
-      formOverlay,
-      form,
-      btnDel,
-    } = phoneBook;
-      // Функционал
-
-    let allRow = renderContacts(list, data);
-    hoverRow(allRow, logo);
-    btnAdd.addEventListener('click', () => {
+  const modalControl = (btnAdd, formOverlay) => {
+    const openModal = () => {
       formOverlay.classList.add('is-visible');
-    });
+    };
+    const closeModal = () => {
+      formOverlay.classList.remove('is-visible');
+    };
+    btnAdd.addEventListener('click', openModal);
+
     formOverlay.addEventListener('click', (e) => {
       const target = e.target;
       if (target === formOverlay ||
         target.classList.contains('close')) {
-        formOverlay.classList.remove('is-visible');
+        closeModal();
       }
     });
+    return {
+      closeModal,
+    };
+  };
+  const deleteControl = (btnDel, list) => {
     btnDel.addEventListener('click', () => {
       document.querySelectorAll('.delete').forEach(del => {
         del.classList.toggle('is-visible');
@@ -258,8 +257,43 @@ const data = [
       const target = e.target;
       if (target.closest('.del-icon')) {
         target.closest('.contact').remove();
-      };
+      }
     });
+  };
+  const addContactPage = (contact, list) => {
+    list.append(createRow(contact));
+  };
+  const formControl = (form, list, closeModal) => {
+    form.addEventListener('submit', e => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const newContact = Object.fromEntries(formData);
+      console.log(newContact);
+      addContactPage(newContact, list);
+      addContactData(newContact);
+      form.reset();
+      closeModal();
+    });
+  };
+  const init = (selectorApp, title) => {
+    const app = document.querySelector(selectorApp);
+    const {
+      list,
+      logo,
+      btnAdd,
+      formOverlay,
+      form,
+      btnDel,
+    } = renderPhoneBook(app, title);
+      // Функционал
+
+    let allRow = renderContacts(list, data);
+    const {closeModal} = modalControl(btnAdd, formOverlay);
+
+    hoverRow(allRow, logo);
+    deleteControl(btnDel, list);
+    formControl(form, list, closeModal);
+    
     list.addEventListener('click', e => {
       const target = e.target;
       if (target.closest('.td_name')) {
@@ -270,6 +304,7 @@ const data = [
         allRow.forEach((contact) => {
           contact.remove();
         });
+        allRow = renderContacts(list, data);
       }
       if (target.closest('.td_surname')) {
         data.sort((prev, next) => {
@@ -279,10 +314,9 @@ const data = [
         allRow.forEach((contact) => {
           contact.remove();
         });
-      }
-      allRow = renderContacts(list, data);
-    })
+        allRow = renderContacts(list, data);
+    }
+    });
   };
-  
   window.phoneBookInit = init;
 };
